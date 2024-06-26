f1820c3edf4cc39e834af1cefdc50674a064080b
Juni Yadi <juniyadi94@gmail.com>
2024-06-26 18:59:37 +0700

    refactor: Remove unused callback route in api.php

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,3 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
-
-
-Route::post('/callback', [OrderController::class, 'callback'])->name('order.callback');
