48f7f783b730b6f9a08a770b1b90c2b1c1147671
Juni Yadi <juniyadi94@gmail.com>
2024-06-26 18:22:23 +0700

    feat: Add search, blog, and news routes to HomeController

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'homepage'])->name('home');
 Route::get('/posts/{slug}', [HomeController::class, 'posts'])->name('posts');
 Route::post('/posts/{slug}/comment', [HomeController::class, 'comment'])->name('posts.comment');
+Route::get('/search', [HomeController::class, 'search'])->name('search');
+Route::get('/blog', [HomeController::class, 'blog'])->name('blog');
+Route::get('/news', [HomeController::class, 'news'])->name('news');
+
+
 
 
 
